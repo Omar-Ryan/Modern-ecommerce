e86e3c915a3d36d4277cfbfa9544235eca952042
Eng Omar <engomaralryan@gmail.com>
2024-09-20 16:11:35 +0300

    update Category: working on redux (dispatch  ,useSelector and many more)

--- src/components/eCommerce/category/Category.tsx
+++ src/components/eCommerce/category/Category.tsx
@@ -1,14 +1,25 @@
-const Category = () => {
+import { Link } from "react-router-dom";
+
+interface CategoryProps {
+  title: string;
+  prefix: string;
+  img: string;
+}
+
+const Category = ({ title, prefix, img }: CategoryProps) => {
   return (
     <div className="w-44 h-44">
-      <div className="overflow-hidden bg-[#f2f2f2] rounded-[50%]">
-        <img
-          src="https://cdn-eu.dynamicyield.com/api/9876644/images/244c68ad42d8b__hp-w12-22032022-h_m-women_shirts-blouses.jpg"
-          alt="Category"
-          className="w-full"
-        />
-      </div>
-      <h4 className="mt-2 text-sm text-center">Title</h4>
+      <Link to={`/categories/products/${prefix}`}>
+        <div className="overflow-hidden bg-[#f2f2f2] rounded-[50%]">
+          <img
+            // src="https://cdn-eu.dynamicyield.com/api/9876644/images/244c68ad42d8b__hp-w12-22032022-h_m-women_shirts-blouses.jpg"
+            src={img}
+            alt={title}
+            className="w-full"
+          />
+        </div>
+        <h4 className="mt-2 text-sm text-center">{title}</h4>
+      </Link>
     </div>
   );
 };
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,24 +1,34 @@
+import { DetailedHTMLProps, HTMLAttributes, useEffect } from "react";
 import { Category } from "../components/eCommerce";
+import actGetCategories from "../store/categories/act/actGetCategories";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 const Categories = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, records } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(actGetCategories());
+  }, [dispatch]);
+
+  const categoriesList: JSX.Element[] | string =
+    records.length > 0
+      ? records.map((record) => (
+          <div
+            key={record.id}
+            className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3"
+          >
+            <Category {...record} />
+          </div>
+        ))
+      : "there are not categories ";
+
   return (
     <div className="container">
       <div className="flex flex-wrap justify-between w-full gap-3">
-        <div className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3">
-          <Category />
-        </div>
-        <div className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3">
-          <Category />
-        </div>
-        <div className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3">
-          <Category />
-        </div>
-        <div className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3">
-          <Category />
-        </div>
-        <div className="flex justify-center mt-2 mb-5 xs:columns-6 md:columns-3">
-          <Category />
-        </div>
+        {categoriesList}
       </div>
     </div>
   );
